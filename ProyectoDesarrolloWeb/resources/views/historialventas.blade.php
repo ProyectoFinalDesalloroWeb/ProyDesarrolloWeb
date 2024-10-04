6a5038cb10d8a8b2fa2b125db5b65817afa51818
Linito26 <110682853+Linito26@users.noreply.github.com>
2024-10-03 20:31:35 -0600

    Añadir las secciones de mensajes de éxito y error para la interacción con el usuario

--- ProyectoDesarrolloWeb/resources/views/historialventas.blade.php
+++ ProyectoDesarrolloWeb/resources/views/historialventas.blade.php
@@ -6,4 +6,23 @@
     <div class="card">
         <div class="container mt-4">
             <h1 class="card-title">Historial de Compras</h1>
+
+                        <!-- Mensaje de éxito -->
+                        @if (session('success'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('success') }}
+                        </div>
+                    @endif
+        
+                    <!-- Mensaje de error -->
+                    @if ($errors->any())
+                        <div class="alert alert-danger" role="alert" style="margin-bottom: 20px;">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+        
 @endsection
