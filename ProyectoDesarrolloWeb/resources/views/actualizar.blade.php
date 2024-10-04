c6c10ae5c6cdd36e1aba3c7681bfb6da8d04b20e
joseprogramacion2 <jvasquezh14@miumg.edu.gt>
2024-10-04 15:01:34 -0600

    modificacion de vista agregar y editar en los campos de texto

--- ProyectoDesarrolloWeb/resources/views/actualizar.blade.php
+++ ProyectoDesarrolloWeb/resources/views/actualizar.blade.php
@@ -3,45 +3,42 @@
 @section('tituloPagina', 'Actualizar')
 
 @section('contenido')
-
-
-
+<br>
     <div class="card">
         <h5 class="card-header">Actualizar materia prima</h5>
         <div class="card-body">
             <p class="card-text">
-            <form action="{{ route('productos.update' , $productos->id) }}" method="POST">
+            <form action="{{ route('productos.update', $productos->id) }}" method="POST" class="formeditar">
                 @csrf
                 @method("PUT")
-                <label for="">Nombre</label>
+                
+                <label class="form-label" for="">Nombre:</label>
                 <input type="text" name="nombre" class="form-control" required value="{{$productos->nombre}}">
 
-                <label for="">Descripcion</label>
+                <label class="form-label" for="">Descripcion:</label>
                 <input type="text" name="descripcion" class="form-control" required value="{{$productos->descripcion}}">
 
-                <label for="unidad_medida">Unidad de medida</label>
+                <label class="form-label" for="unidad_medida">Unidad de medida:</label>
                 <select name="unidad_medida" id="unidad_medida" class="form-control" required>
-                    <option value="kilogramos" {{ $productos->unidad_medida == 'kilogramos' ? 'selected' : '' }}>Kilogramos
-                    </option>
+                    <option value="kilogramos" {{ $productos->unidad_medida == 'kilogramos' ? 'selected' : '' }}>Kilogramos</option>
                     <option value="gramos" {{ $productos->unidad_medida == 'gramos' ? 'selected' : '' }}>Gramos</option>
                     <option value="litros" {{ $productos->unidad_medida == 'litros' ? 'selected' : '' }}>Litros</option>
-                    <option value="mililitros" {{ $productos->unidad_medida == 'mililitros' ? 'selected' : '' }}>Mililitros
-                    </option>
+                    <option value="mililitros" {{ $productos->unidad_medida == 'mililitros' ? 'selected' : '' }}>Mililitros</option>
                 </select>
 
-                <label for="">Cantidad</label>
+                <label class="form-label" for="">Cantidad:</label>
                 <input type="text" name="cantidad" class="form-control" required value="{{$productos->cantidad}}">
 
-                <label for="">Precio unitario</label>
+                <label class="form-label" for="">Precio unitario:</label>
                 <input type="text" name="precio_unitario" class="form-control" required value="{{$productos->precio_unitario}}">
 
-                <label for="">Proveedor</label>
+                <label class="form-label" for="">Proveedor:</label>
                 <input type="text" name="proveedor" class="form-control" required value="{{$productos->proveedor}}">
 
-                <label for="">Fecha de adquisicion</label>
+                <label class="form-label" for="">Fecha de adquisicion:</label>
                 <input type="date" name="fecha_adquisicion" class="form-control" required value="{{$productos->fecha_adquisicion}}">
 
-                <label for="">Fecha de expiracion</label>
+                <label class="form-label" for="">Fecha de expiracion:</label>
                 <input type="date" name="fecha_expiracion" class="form-control" required value="{{$productos->fecha_expiracion}}">
 
                 <br>
@@ -51,10 +48,8 @@
                 <button class="btn btn-warning">
                     <span class="fa-solid fa-sync-alt"></span> Actualizar
                 </button>
-                
             </form>
             </p>
-
         </div>
     </div>
 @endsection
--- ProyectoDesarrolloWeb/resources/views/agregar.blade.php
+++ ProyectoDesarrolloWeb/resources/views/agregar.blade.php
@@ -3,56 +3,51 @@
 @section('tituloPagina', 'Insertar')
 
 @section('contenido')
-
-
     <br>
     <div class="card">
         <h5 class="card-header">Agregar nueva materia prima</h5>
         <div class="card-body">
-
             <p class="card-text">
-            <form action="{{ route('productos.store') }}" method="POST">
-                @csrf
-                <label for="">Nombre</label>
-                <input type="text" name="nombre" class="form-control" required>
+                <form action="{{ route('productos.store') }}" method="POST" class="formagregar">
+                    @csrf
+                    <label class="form-label" for="">Nombre:</label>
+                    <input type="text" name="nombre" class="form-control" required>
 
-                <label for="">Descripcion</label>
-                <input type="text" name="descripcion" class="form-control" required>
+                    <label class="form-label" for="">Descripción:</label>
+                    <input type="text" name="descripcion" class="form-control" required>
 
-                <label for="unidad_medida">Unidad de medidad</label>
-                <select name="unidad_medida" class="form-control" required>
-                    <option value="kilogramos">Kilogramos</option>
-                    <option value="gramos">Gramos</option>
-                    <option value="litros">Litros</option>
-                    <option value="mililitros">Mililitros</option>
-                </select>
+                    <label class="form-label" for="unidad_medida">Unidad de medida:</label>
+                    <select name="unidad_medida" class="form-control" required>
+                        <option value="kilogramos">Kilogramos</option>
+                        <option value="gramos">Gramos</option>
+                        <option value="litros">Litros</option>
+                        <option value="mililitros">Mililitros</option>
+                    </select>
 
-                <label for="">Cantidad</label>
-                <input type="text" name="cantidad" class="form-control" required>
+                    <label class="form-label" for="">Cantidad:</label>
+                    <input type="text" name="cantidad" class="form-control" required>
 
-                <label for="">Precio unitario</label>
-                <input type="text" name="precio_unitario" class="form-control" required>
+                    <label class="form-label" for="">Precio unitario:</label>
+                    <input type="text" name="precio_unitario" class="form-control" required>
 
-                <label for="">Proveedor</label>
-                <input type="text" name="proveedor" class="form-control" required>
+                    <label class="form-label" for="">Proveedor:</label>
+                    <input type="text" name="proveedor" class="form-control" required>
 
-                <label for="">Fecha de adquisicion</label>
-                <input type="date" name="fecha_adquisicion" class="form-control" required>
+                    <label class="form-label" for="">Fecha de adquisición:</label>
+                    <input type="date" name="fecha_adquisicion" class="form-control" required>
 
-                <label for="">Fecha de expiracion</label>
-                <input type="date" name="fecha_expiracion" class="form-control" required>
+                    <label class="form-label" for="">Fecha de expiración:</label>
+                    <input type="date" name="fecha_expiracion" class="form-control" required>
 
-                <br>
-                <a href="{{ route('productos.index') }}" class="btn btn-info">
-                    <span class="fas fa-undo"></span> Regresar
-                </a>
-                <button class="btn btn-primary">
-                    <span class="fas fa-plus"></span> Agregar
-                </button>
-
-            </form>
+                    <br>
+                    <a href="{{ route('productos.index') }}" class="btn btn-info">
+                        <span class="fas fa-undo"></span> Regresar
+                    </a>
+                    <button class="btn btn-primary">
+                        <span class="fas fa-plus"></span> Agregar
+                    </button>
+                </form>
             </p>
-
         </div>
     </div>
 @endsection
