ed1f30e8e538e2dc5cac1b819a48373a3dcd0dee
Linito26 <110682853+Linito26@users.noreply.github.com>
2024-10-03 20:30:24 -0600

    Creacion de la vista Historial de Compras

---
+++ ProyectoDesarrolloWeb/resources/views/historialventas.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.plantillaclientes')
+
+@section('tituloPagina', 'Historial de Compras')
+
+@section('contenido')
+    <div class="card">
+        <div class="container mt-4">
+            <h1 class="card-title">Historial de Compras</h1>
+@endsection
