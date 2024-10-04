5d59198e2c94b88e7c74133ef6bbf413f355f9ae
Linito26 <110682853+Linito26@users.noreply.github.com>
2024-10-03 20:33:12 -0600

    Incluir los formularios de búsqueda y el botón de Mostrar Todos

--- ProyectoDesarrolloWeb/resources/views/historialventas.blade.php
+++ ProyectoDesarrolloWeb/resources/views/historialventas.blade.php
@@ -24,5 +24,20 @@
                             </ul>
                         </div>
                     @endif
-        
+                                <!-- Formulario de búsqueda y botón Mostrar Todos en la misma línea -->
+            <div class="mb-3 d-flex justify-content-between">
+                <form method="GET" action="{{ route('ventas.buscar') }}" class="d-flex align-items-center">
+                    <input type="hidden" name="query" value="">
+                    <button class="btn btn-primary me-2" type="submit">Mostrar Todos</button>
+                </form>
+
+                <form method="GET" action="{{ route('ventas.buscar') }}" class="d-flex align-items-center">
+                    <input type="text" id="search" class="form-control me-2" name="query"
+                        placeholder="Buscar por nombre o código..." required value="{{ request('query') }}">
+                    <button type="submit" class="btn btn-primary d-flex align-items-center">
+                        <span class="fa-solid fa-magnifying-glass me-2"></span> Buscar
+                    </button>
+                </form>
+            </div>
+
 @endsection
